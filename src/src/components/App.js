e11a1f4226d372ea098e44e5f6ab2f9e401b525f
Sittha Darapisut <katoretv@gmail.com>
2024-08-01 15:22:32 +0700

    v1.0.0

--- src/src/components/App.js
+++ src/src/components/App.js
@@ -23,6 +23,7 @@ import Class        from '../pages/ST/Class';
 import Lab          from '../pages/ST/Lab';
 import Portfolio    from '../pages/ST/Portfolio';
 
+
 const isDev     = process.env.REACT_APP_DEV.toLowerCase() === 'true';
 const Login     = isDev ? require('../test/test-login').default : require('../pages/Login').default;
 const Callback  = isDev ? require('../test/test-callback').default : require('../pages/Callback').default;
